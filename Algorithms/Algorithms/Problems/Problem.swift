ce444f04d5bfa1780b83e28fc04bd7ef5ae5fbcf
NowEatS <seotw93@gmail.com>
2021-12-02 20:37:41 +0900

    Algorithm 프로젝트 생성

---
+++ Algorithms/Algorithms/Problems/Problem.swift
@@ -0,0 +1,12 @@
+//
+//  Problem.swift
+//  Algorithms
+//
+//  Created by TaeWon Seo on 2021/12/02.
+//
+
+import Foundation
+
+protocol Problem {
+    func playAnswer()
+}
---
+++ Algorithms/Algorithms/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithms
+//
+//  Created by TaeWon Seo on 2021/12/02.
+//
+
+import Foundation
+
+var problem: Problem
